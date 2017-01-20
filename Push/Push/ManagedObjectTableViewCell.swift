da190cfc78cbb305aa371ccd6fd45be2293efd81
Jordan Zucker <jordan.zucker@gmail.com>
2017-01-19 17:08:02 -0800

    Started refactoring classes and cleaning up project

--- Push/Push/ManagedObjectTableViewCell.swift
+++ Push/Push/ManagedObjectTableViewCell.swift
@@ -27,35 +27,3 @@ class ManagedObjectTableViewCell: UITableViewCell {
     }
 
 }
-
-class ResultTableViewCell: ManagedObjectTableViewCell {
-    let textView: UITextView
-    
-    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
-        self.textView = UITextView(frame: .zero)
-        super.init(style: style, reuseIdentifier: reuseIdentifier)
-        textView.forceAutoLayout()
-        contentView.addSubview(textView)
-        let widthConstraint = NSLayoutConstraint(item: textView, attribute: .width, relatedBy: .equal, toItem: contentView, attribute: .width, multiplier: 1.0, constant: 0.0)
-        let heightConstraint = NSLayoutConstraint(item: textView, attribute: .height, relatedBy: .equal, toItem: contentView, attribute: .height, multiplier: 1.0, constant: 0.0)
-        let centerXConstraint = NSLayoutConstraint(item: textView, attribute: .centerX, relatedBy: .equal, toItem: contentView, attribute: .centerX, multiplier: 1.0, constant: 0.0)
-        let centerYConstraint = NSLayoutConstraint(item: textView, attribute: .centerY, relatedBy: .equal, toItem: contentView, attribute: .centerY, multiplier: 1.0, constant: 0.0)
-        NSLayoutConstraint.activate([widthConstraint, heightConstraint, centerXConstraint, centerYConstraint])
-    }
-    
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    override func update(with object: NSManagedObject) {
-        guard let result = object as? Result else {
-            fatalError("Failed to convert: \(object.debugDescription)")
-        }
-        textView.text = result.textViewDisplayText
-    }
-    
-    override func prepareForReuse() {
-        super.prepareForReuse()
-        textView.text = nil
-    }
-}
---
+++ Push/Push/ResultTableViewCell.swift
@@ -0,0 +1,42 @@
+//
+//  ResultTableViewCell.swift
+//  Push
+//
+//  Created by Jordan Zucker on 1/19/17.
+//  Copyright © 2017 PubNub. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class ResultTableViewCell: ManagedObjectTableViewCell {
+    let textView: UITextView
+    
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        self.textView = UITextView(frame: .zero)
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        textView.forceAutoLayout()
+        contentView.addSubview(textView)
+        let widthConstraint = NSLayoutConstraint(item: textView, attribute: .width, relatedBy: .equal, toItem: contentView, attribute: .width, multiplier: 1.0, constant: 0.0)
+        let heightConstraint = NSLayoutConstraint(item: textView, attribute: .height, relatedBy: .equal, toItem: contentView, attribute: .height, multiplier: 1.0, constant: 0.0)
+        let centerXConstraint = NSLayoutConstraint(item: textView, attribute: .centerX, relatedBy: .equal, toItem: contentView, attribute: .centerX, multiplier: 1.0, constant: 0.0)
+        let centerYConstraint = NSLayoutConstraint(item: textView, attribute: .centerY, relatedBy: .equal, toItem: contentView, attribute: .centerY, multiplier: 1.0, constant: 0.0)
+        NSLayoutConstraint.activate([widthConstraint, heightConstraint, centerXConstraint, centerYConstraint])
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func update(with object: NSManagedObject) {
+        guard let result = object as? Result else {
+            fatalError("Failed to convert: \(object.debugDescription)")
+        }
+        textView.text = result.textViewDisplayText
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        textView.text = nil
+    }
+}
