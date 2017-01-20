981e01f3bc4ff2f2ec17edc954d52046b8187a41
Jordan Zucker <jordan.zucker@gmail.com>
2017-01-19 16:06:58 -0800

    Cleaned up lots of comments

--- Push/Push/Network.swift
+++ Push/Push/Network.swift
@@ -27,21 +27,6 @@ class Network: NSObject, PNObjectEventListener {
     
     var client: PubNub!
     
-//    func setCurrentUser(user: User?) {
-//        guard let actualUser = user else {
-//            DispatchQueue.main.async {
-//                self.user = nil
-//            }
-//            return
-//        }
-//        DispatchQueue.main.async {
-//            guard let contextualUser = self.networkContext.object(with: actualUser.objectID) as? User else {
-//                fatalError()
-//            }
-//            self.user = contextualUser
-//        }
-//    }
-    
     private var _user: User?
     
     public var user: User? {
@@ -87,28 +72,6 @@ class Network: NSObject, PNObjectEventListener {
             
         }
     }
-    
-//    private var user: User? {
-//        didSet {
-//            if let existingOldValue = oldValue {
-//                existingOldValue.removeObserver(self, forKeyPath: #keyPath(User.pushToken), context: &networkKVOContext)
-//                existingOldValue.removeObserver(self, forKeyPath: #keyPath(User.pushChannels), context: &networkKVOContext)
-//            }
-//            user?.addObserver(self, forKeyPath: #keyPath(User.pushToken), options: [.new, .old, .initial], context: &networkKVOContext)
-//            user?.addObserver(self, forKeyPath: #keyPath(User.pushChannels), options: [.new, .old, .initial], context: &networkKVOContext)
-//            guard let currentUser = user else {
-//                return
-//            }
-//            networkContext.perform {
-//                let identifier = currentUser.identifier!
-//                DispatchQueue.main.async {
-//                    let configuration = self.config(with: identifier)
-//                    self.client = PubNub.clientWithConfiguration(configuration, callbackQueue: self.networkQueue)
-//                    self.client.addListener(self)
-//                }
-//            }
-//        }
-//    }
     
     deinit {
         user = nil
@@ -177,12 +140,6 @@ class Network: NSObject, PNObjectEventListener {
                 self.updatePush(tokens: (oldValue, newValue), current: self._pushChannels)
             }
             networkQueue.async(execute: setItem)
-//            let updatePushTokenItem = DispatchWorkItem(qos: .utility, flags: []) {
-//                print("pushToken: updateItem")
-////                self.updatePushToken(oldToken: oldValue, newToken: newValue)
-//                self.updatePush(tokens: (oldValue, newValue), current: self._pushChannels)
-//            }
-//            networkQueue.async(execute: updatePushTokenItem)
         }
         
         get {
@@ -193,11 +150,6 @@ class Network: NSObject, PNObjectEventListener {
             networkQueue.sync(execute: getItem)
             return finalToken
         }
-//        didSet {
-//            print("set pushToken: \(pushToken)")
-//            // now remove old token and update new token
-//            updatePushToken(oldToken: oldValue, newToken: pushToken)
-//        }
     }
     
     var _pushChannels: Set<String>? = nil
@@ -213,11 +165,6 @@ class Network: NSObject, PNObjectEventListener {
                 self.updatePush(channels: (oldValue, newValue), current: self._pushToken)
             }
             networkQueue.async(execute: setItem)
-//            let updatePushItem = DispatchWorkItem(qos: .utility, flags: []) {
-//                print("pushChannels: updatePushItem")
-//                
-//            }
-//            networkQueue.async(execute: updatePushItem)
         }
         
         get {
@@ -229,12 +176,6 @@ class Network: NSObject, PNObjectEventListener {
             return finalChannels
         }
     }
-    
-//    var pushChannels: Set<String>? = Set() {
-//        didSet {
-//            updatePush(oldChannels: oldValue, newChannels: pushChannels)
-//        }
-//    }
     
     typealias tokens = (oldToken: Data?, newToken: Data?)
     typealias channels = (oldChannels: Set<String>?, newChannels: Set<String>?)
@@ -317,123 +258,6 @@ class Network: NSObject, PNObjectEventListener {
             }
         }
     }
-    
-//    func updatePush(tokens: tokens, channels: channels) {
-//        
-//        let pushCompletionBlock: PNPushNotificationsStateModificationCompletionBlock = { (status) in
-//            self.networkContext.perform {
-//                let _ = ResultType.createCoreDataObject(result: status, in: self.networkContext)
-//                do {
-//                    try self.networkContext.save()
-//                } catch {
-//                    fatalError(error.localizedDescription)
-//                }
-//            }
-//        }
-//        
-//        var addingChannels: Set<String> = Set()
-//        var removingChannels: Set<String> = Set()
-//        
-//        if let newChannels = channels.newChannels {
-//            addingChannels.formUnion(newChannels)
-//        }
-//        if let oldChannels = channels.oldChannels {
-//            addingChannels.subtract(oldChannels)
-//            
-//        }
-//        
-//        let addChannelsArray = channelsArray(for: addingChannels)
-//        let removingChannelsArray = channelsArray(for: removingChannels)
-//        
-//        switch (tokens, channels) {
-//        case ((nil, nil), (_, _)):
-//            break
-//        case let ((oldToken, nil), (_, _)) where oldToken != nil:
-//            client.removeAllPushNotificationsFromDeviceWithPushToken(oldToken!, andCompletion: pushCompletionBlock)
-//        // oldToken is already not nil for this case
-//        case let ((oldToken, newToken), (_, _)) where newToken != nil:
-//            if let existingOldToken = oldToken,  {
-//                client.removePushNotificationsFromChannels(<#T##channels: [String]##[String]#>, withDevicePushToken: <#T##Data#>, andCompletion: <#T##PNPushNotificationsStateModificationCompletionBlock?##PNPushNotificationsStateModificationCompletionBlock?##(PNAcknowledgmentStatus) -> Void#>)
-//            }
-//        default:
-//            fatalError("Can't handle this case")
-//        }
-//    }
-//    
-//    func updatePush(oldChannels: Set<String>?, newChannels: Set<String>?) {
-//        print("%%%%%%%%%%%%%%%% \(#function) with old: \(oldChannels) and new: \(newChannels)")
-//        // seems like this could result in tokens not being removed
-//        guard let currentPushToken = pushToken else {
-//            return
-//        }
-//        
-//        let newPushArray = channelsArray(for: newChannels)
-//        let oldPushArray = channelsArray(for: oldChannels)
-//        
-//        let pushCompletionBlock: PNPushNotificationsStateModificationCompletionBlock = { (status) in
-//            self.networkContext.perform {
-//                let _ = ResultType.createCoreDataObject(result: status, in: self.networkContext)
-//                do {
-//                    try self.networkContext.save()
-//                } catch {
-//                    fatalError(error.localizedDescription)
-//                }
-//            }
-//        }
-//        
-//        switch (oldChannels, newChannels) {
-//        case (nil, nil):
-//            break
-//        case let (oldChannels, nil) where oldChannels != nil:
-//            client.removePushNotificationsFromChannels(oldPushArray!, withDevicePushToken: currentPushToken, andCompletion: pushCompletionBlock)
-//        case let (nil, newChannels) where newChannels != nil:
-//            client.addPushNotificationsOnChannels(newPushArray!, withDevicePushToken: currentPushToken, andCompletion: pushCompletionBlock)
-//        case let (oldChannels, newChannels) where (oldChannels != nil) && (newChannels != nil):
-//            if oldChannels != newChannels {
-//                client.removePushNotificationsFromChannels(oldPushArray!, withDevicePushToken: currentPushToken, andCompletion: pushCompletionBlock)
-//            }
-//            client.addPushNotificationsOnChannels(newPushArray!, withDevicePushToken: currentPushToken, andCompletion: pushCompletionBlock)
-//        default:
-//            fatalError("unexpected case")
-//        }
-//    }
-//    
-//    func updatePushToken(oldToken: Data?, newToken: Data?) {
-//        // seems like this could result in tokens not being removed
-//        guard let currentChannelsArray = channelsArray(for: pushChannels) else {
-//            return
-//        }
-//        
-//        let pushCompletionBlock: PNPushNotificationsStateModificationCompletionBlock = { (status) in
-//            self.networkContext.perform {
-//                let _ = ResultType.createCoreDataObject(result: status, in: self.networkContext)
-//                do {
-//                    try self.networkContext.save()
-//                } catch {
-//                    fatalError(error.localizedDescription)
-//                }
-//            }
-//        }
-//        
-//        switch (oldToken, newToken) {
-//        case (nil, nil):
-//            break
-//        case let (oldToken, nil) where oldToken != nil:
-//            print("whatever")
-//            client.removeAllPushNotificationsFromDeviceWithPushToken(oldToken!, andCompletion: pushCompletionBlock)
-//        case let (nil, newToken) where newToken != nil:
-//            client.addPushNotificationsOnChannels(currentChannelsArray, withDevicePushToken: newToken!, andCompletion: pushCompletionBlock)
-//            print("again")
-//        case let (oldToken, newToken) where (oldToken != nil) && (newToken != nil):
-//            print("hey hey")
-//            if oldToken != newToken {
-//                client.removeAllPushNotificationsFromDeviceWithPushToken(oldToken!, andCompletion: pushCompletionBlock)
-//            }
-//            client.addPushNotificationsOnChannels(currentChannelsArray, withDevicePushToken: newToken!, andCompletion: pushCompletionBlock)
-//        default:
-//            fatalError("unexpected case")
-//        }
-//    }
     
     func channelsArray(for set: Set<String>?) -> [String]? {
         guard let actualSet = set, !actualSet.isEmpty else {
