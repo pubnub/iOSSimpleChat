9000c6a0f94dcaa6404e5b7f6df23e5ddad5a2c3
Jordan Zucker <jordan.zucker@gmail.com>
2017-01-25 13:47:27 -0800

    Cleaned up unused classes

--- Push/Push/KeyValueLabelCollectionViewCell.swift
+++ Push/Push/KeyValueLabelCollectionViewCell.swift
@@ -1,8 +1,8 @@
 //
-//  TitleContentsCollectionViewCell.swift
+//  KeyValueLabelCollectionViewCell.swift
 //  Push
 //
-//  Created by Jordan Zucker on 1/24/17.
+//  Created by Jordan Zucker on 1/25/17.
 //  Copyright © 2017 PubNub. All rights reserved.
 //
 
@@ -59,78 +59,11 @@ protocol KeyValue {
 }
 
 extension KeyValue {
-
+    
     var debugDescription: String {
         return "key: " + displayKeyName + "; value: " + (displayValue ?? "")
     }
 }
-
-enum ConfigurationProperty: KeyValue, KeyValueAlertControllerUpdates {
-    case origin(PNConfiguration)
-    case publishKey(PNConfiguration)
-    case subscribeKey(PNConfiguration)
-    case authKey(PNConfiguration)
-    case uuid(PNConfiguration)
-    
-    var key: String {
-        switch self {
-        case .origin(_):
-            return #keyPath(PNConfiguration.origin)
-        case .publishKey(_):
-            return #keyPath(PNConfiguration.publishKey)
-        case .subscribeKey(_):
-            return #keyPath(PNConfiguration.subscribeKey)
-        case .authKey(_):
-            return #keyPath(PNConfiguration.authKey)
-        case .uuid(_):
-            return #keyPath(PNConfiguration.uuid)
-        }
-    }
-    
-    var displayKeyName: String {
-        switch self {
-        case .origin:
-            return "Origin"
-        case .publishKey:
-            return "Publish Key"
-        case .subscribeKey:
-            return "Subscribe Key"
-        case .authKey:
-            return "Auth Key"
-        case .uuid:
-            return "UUID"
-        }
-    }
-    
-    var value: Any? {
-        get {
-            switch self {
-            case let .publishKey(config), let .origin(config), let .subscribeKey(config), let .authKey(config), let .uuid(config):
-                return config.value(forKey: key)
-            }
-        }
-        set {
-            switch self {
-            case let .publishKey(config), let .origin(config), let .subscribeKey(config), let .authKey(config), let .uuid(config):
-                config.setValue(newValue, forKey: key)
-            }
-        }
-    }
-    
-    var displayValue: String? {
-        switch self {
-        case .subscribeKey(_), .publishKey(_), .origin(_), .authKey(_), .uuid(_):
-            return value as! String? // we should never fail for these values
-        }
-    }
-    
-    
-}
-
-//struct ConfigurationProperty: KeyValue {
-//    let key: String
-//    
-//}
 
 protocol KeyValueCell {
     func update(with item: KeyValue)
@@ -154,15 +87,10 @@ class KeyValueLabelCollectionViewCell: UICollectionViewCell, KeyValueCell {
         contentView.addSubview(keyLabel)
         contentView.addSubview(valueLabel)
         contentView.backgroundColor = .red
-//        let widthConstraint = NSLayoutConstraint(item: textView, attribute: .width, relatedBy: .equal, toItem: contentView, attribute: .width, multiplier: 1.0, constant: 0.0)
-//        let heightConstraint = NSLayoutConstraint(item: textView, attribute: .height, relatedBy: .equal, toItem: contentView, attribute: .height, multiplier: 1.0, constant: 0.0)
-//        let centerXConstraint = NSLayoutConstraint(item: textView, attribute: .centerX, relatedBy: .equal, toItem: contentView, attribute: .centerX, multiplier: 1.0, constant: 0.0)
-//        let centerYConstraint = NSLayoutConstraint(item: textView, attribute: .centerY, relatedBy: .equal, toItem: contentView, attribute: .centerY, multiplier: 1.0, constant: 0.0)
-//        NSLayoutConstraint.activate([widthConstraint, heightConstraint, centerXConstraint, centerYConstraint])
         let views = [
             "keyLabel": keyLabel,
             "valueLabel": valueLabel,
-        ]
+            ]
         let heightConstraints = NSLayoutConstraint.constraints(withVisualFormat: "V:|[keyLabel][valueLabel]|", options: [], metrics: nil, views: views)
         let titleWidthConstraints = NSLayoutConstraint.constraints(withVisualFormat: "H:|[keyLabel]|", options: [], metrics: nil, views: views)
         let contentsWidthConstraints = NSLayoutConstraint.constraints(withVisualFormat: "H:|[valueLabel]|", options: [], metrics: nil, views: views)
